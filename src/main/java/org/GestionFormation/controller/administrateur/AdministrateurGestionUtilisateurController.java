fa2bddafcd10b146dddc0b600fea86fa0a5be843
FMinus <ayoub.deqqaq@gmail.com>
2016-09-09 18:22:16 +0100

    working login/logout added user roles working roles added sessions formation about to change table model to joined

--- src/main/java/org/GestionFormation/controller/administrateur/AdministrateurGestionUtilisateurController.java
+++ src/main/java/org/GestionFormation/controller/administrateur/AdministrateurGestionUtilisateurController.java
@@ -21,10 +21,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- *
- * @author Ayoub
- */
+
 @Controller
 @RequestMapping("/admin")
 public class AdministrateurGestionUtilisateurController
